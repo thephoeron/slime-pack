bf8462a3dd7eea0fa2296f7a971e399a2ac6a935
the Phoeron <thephoeron@me.com>
2014-07-17 08:26:18 -0400

    add slime-mode to ac-modes

--- config/slime.el
+++ config/slime.el
@@ -9,4 +9,5 @@
 (add-hook 'slime-mode-hook 'set-up-slime-ac)
 (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
 (eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'slime-mode)
   '(add-to-list 'ac-modes 'slime-repl-mode))
