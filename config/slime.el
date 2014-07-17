9c9f631f43b8419e85595ca7773166b59593a00e
the Phoeron <thephoeron@me.com>
2014-07-17 16:25:57 -0400

    Add emacs-neotree and multi-term packages just for convenience.

--- config/slime.el
+++ config/slime.el
@@ -10,3 +10,11 @@
 (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
 (eval-after-load "auto-complete"
   '(add-to-list 'ac-modes '(slime-mode slime-repl-mode)))
+
+(live-add-pack-lib "emacs-neotree")
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+
+;(live-add-pack-lib "multiterm")
+(require 'multi-term)
+(setq multi-term-program "/bin/bash")
