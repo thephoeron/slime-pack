b55022c238fe15f605254e956b0eea520d1a5b3b
the Phoeron <thephoeron@me.com>
2014-07-17 08:31:35 -0400

    bugfix on config/slime.el

--- config/slime.el
+++ config/slime.el
@@ -9,5 +9,4 @@
 (add-hook 'slime-mode-hook 'set-up-slime-ac)
 (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
 (eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'slime-mode)
-  '(add-to-list 'ac-modes 'slime-repl-mode))
+  '(add-to-list 'ac-modes '(slime-mode slime-repl-mode)))
