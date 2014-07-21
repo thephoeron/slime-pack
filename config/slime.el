eb4efa907677355c57e2dce2ab1592d26304edc9
the Phoeron <thephoeron@me.com>
2014-07-21 00:48:30 -0400

    mmm-mode not working ideally, comment out from config/slime.el for now

--- config/slime.el
+++ config/slime.el
@@ -18,15 +18,15 @@
 (require 'multi-term)
 (setq multi-term-program "/bin/bash")
 
-(live-add-pack-lib "mmm-mode")
-(require 'mmm-mode)
+;(live-add-pack-lib "mmm-mode")
+;(require 'mmm-mode)
 
-(mmm-add-classes
-  '((markdown-lisp
-     :submode lisp-mode
-     :face mmm-declaration-submode-face
-     :front "^```lisp[\n\r]+"
-     :back "^```$")))
-
-(setq mmm-global-mode 't)
-(mmm-add-mode-ext-class 'markdown-mode nil 'markdown-lisp)
+;(mmm-add-classes
+;  '((markdown-lisp
+;     :submode lisp-mode
+;     :face mmm-declaration-submode-face
+;     :front "^```lisp[\n\r]+"
+;     :back "^```$")))
+;
+;(setq mmm-global-mode 't)
+;(mmm-add-mode-ext-class 'markdown-mode nil 'markdown-lisp)
